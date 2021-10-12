89667406d7a2bb62640728c1b2fb9d74d3110d07
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-12 21:12:01 +1300

    added api key with example

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,11 @@ import React from 'react'
 
 function App () {
 
+  const API = {
+    key: '687bdd4bcdfaae0e53254b01661b9ec2',
+    base: `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}` // example GET request
+  }
+
   return (
     <>
       <div className='app'>
