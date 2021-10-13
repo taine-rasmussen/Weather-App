d01253f0e0bdad8531afaa9f5e854f846ed62677
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-13 14:26:22 +1300

    getWeather func gets all weather data and updates state with it

--- client/api.js
+++ client/api.js
@@ -1,7 +1,9 @@
 import request from 'superagent'
 
-export function getData () {
+export function getData (city) {
   return request
     .get(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=687bdd4bcdfaae0e53254b01661b9ec2`)
     .then(res => res.body)
 }
+
+
