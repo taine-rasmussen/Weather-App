9fff7228bd7247e9754c9aa6190d0f68231f40b6
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-14 14:25:05 +1300

    refactored display component with conditional rendering to fix issue of undefined objects from aysnc data

--- client/components/Display.jsx
+++ client/components/Display.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 const Display = ({allData}) => {
 
 	return(
-		<div className="display-container">
-			<div className="display-left-section">
-				<h2>{allData.name}</h2>
-			</div>
-			<div className="display-right-section"> 
-				<h3>{allData.timezone}</h3>
-			</div>
+		<div>
+			{allData.name ? 
+				<div className="display-container">
+					<div className="display-left-section">
+						<h2>{}</h2>
+					</div>
+					<div className="display-right-section"> 
+						<h3>{allData.name}, {allData.sys.country}</h3>
+					</div>
+				</div> : null}
 		</div>
+
 	)
 }
 
