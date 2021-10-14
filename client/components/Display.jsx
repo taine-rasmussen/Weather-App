d42cc2086c3e484e7519d6b0ef732b4d831111f2
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-14 14:37:00 +1300

    added and styled bg img for display left section

--- client/components/Display.jsx
+++ client/components/Display.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const Display = ({allData}) => {
 
+	const mtnsDay = 'imgs/mtns-day.jpeg'
+	const mtnsNight = 'imgs/mtns-nigth.jpeg'
+
+
 	return(
 		<div>
 			{allData.name ? 
 				<div className="display-container">
 					<div className="display-left-section">
+						<img src={mtnsDay} className="left-bg-img"/>
 						<h2>{allData.weather[0].description}</h2>
 					</div>
 					<div className="display-right-section"> 
